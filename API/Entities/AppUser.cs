2c2340d10b23d52e7198edc147f6228643e271da
Federico Matos <federicomatos@icloud.com>
2021-03-04 22:03:28 -0400

    Initial commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public AppUser()
+        {
+        }
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
